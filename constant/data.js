1ea50829af38f574087b00bc5cc00313a844f39c
Aniket Panchal <39687408+aapanchal@users.noreply.github.com>
2024-03-26 01:19:57 +0530

    data added

--- constant/data.js
+++ constant/data.js
@@ -1,80 +1,86 @@
 const userData = {
-    githubUsername: "aapanchal",
-    name: "Aniket Panchal",
-    designation: "Frontend Developer",
-    avatarUrl: "/avatar.png",
-    email: "aniketpanchal.5097@gmail.com",
-    phone: "+91 8200497629",
-    address: "Anand, Gujarat, India.",
-    projects: [
-      {
-        id:"001",
-        title: "Spotify Clone",
-        link: "https://spotify-clone-new-ten.vercel.app/",
-        imgUrl: "/Spotify-web-player.jpg",
-      },
-      {
-        title: "Portfolio",
-        link: "https://placeholdertech.in",
-        imgUrl: "/Portfolio.png",
-      },
-    
-    ],
-    about: {
-      title:
-        "I'm a front-end developer that loves building products and web applications",
-      description: [
-        `I develop websites with HTML, CSS and JavaScript. I have top skills in using design softwares like Figma, Adobe XD and PhotoShop; with prototype tools like Figma and Protopie.`,
-        `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`
-      ],
-      currentProject: "Tailwind Master Kit",
-      currentProjectUrl: "https://tailwindmasterkit.com",
+  githubUsername: "aapanchal",
+  name: "Aniket Panchal",
+  designation: "Frontend Developer",
+  avatarUrl: "/avatar.png",
+  email: "aniketpanchal.5097@gmail.com",
+  phone: "+91 8200497629",
+  address: "Anand, Gujarat, India.",
+  projects: [
+    {
+      id: "001",
+      title: "Spotify Clone",
+      link: "https://spotify-clone-new-ten.vercel.app/",
+      imgUrl: "/Spotify-web-player.jpg",
     },
-    experience: [
-      {
-        id:"001",
-        title: "Front End Developer",
-        company: "Thinksense.ai",
-        year: "2019",
-        companyLink: "Thinksense.ai",
-        desc: "Contributed to our flagship product - APR-AI - which is an AI powered Automated Process Recognation product.",
-      },
-   
-      {
-        id:"002",
-        title: "Internship",
-        company: "Thinksense.ai",
-        year: "2019",
-        companyLink: "Thinksense.ai",
-        desc: "Developed Smart Sense Report system with Node.js and Sails.js, detect employee productivity.",
-      },
-      {
-        id:"003",
-        title: "Graduation",
-        company: "Maharaja Sayajirao University, Vadodara, Gujrat",
-        year: "2019",
-        companyLink: "https://www.msubaroda.ac.in/",
-        desc: "Major in Computer Science and Engineering with a CGPA of 5.0. ",
-      },
-      {
-        id:"004",
-        title: "High School",
-        company: "Anand High School, Anand.",
-        year: "2014",
-        companyLink: "https://www.srksm.org/highschool",
-        desc: "Subjects being PCM - I barely survived with 50.5% marks aggregate.",
-      },
-    ],
-    resumeUrl:
-      "https://drive.google.com/file/d/1IaNQGWyRfSZCdiuxhsfOgr8yIFUBz79U/view?usp=sharing",
-    socialLinks: {
-      instagram: "https://instagram.com/aniket28__",
-      twitter: "https://twitter.com/Aniketpanchal28",
-      linkedin: "https://www.linkedin.com/in/aniket-panchal-b11720108",
-      github: "https://github.com/aapanchal",
-      facebook: "https://facebook.com/",
+    {
+      title: "Portfolio",
+      link: "https://placeholdertech.in",
+      imgUrl: "/Portfolio.png",
     },
-  };
-  
-  export default userData;
-  
+  ],
+  about: {
+    title:
+      "I'm a front-end developer that loves building products and web applications",
+    description: [
+      `I develop websites with HTML, CSS and JavaScript. I have top skills in using design softwares like Figma, Adobe XD and PhotoShop; with prototype tools like Figma and Protopie.`,
+      `After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      id: "001",
+      title: "Software Developer",
+      company: "Infosys",
+      year: "Present",
+      companyLink: "Infosys.com",
+      desc: "Microsoft Supply Chain Project. It is built on Angular, typescript, MySQL. I’ve worked on Onboard and Account Management Module. also worked on solarwinds project (react.js)",
+    },
+    {
+      id: "002",
+      title: "Front End Developer",
+      company: "Thinksense.ai",
+      year: "2019",
+      companyLink: "Thinksense.ai",
+      desc: "Contributed to our flagship product - APR-AI - which is an AI powered Automated Process Recognation product.",
+    },
+
+    {
+      id: "003",
+      title: "Internship",
+      company: "Thinksense.ai",
+      year: "2019",
+      companyLink: "Thinksense.ai",
+      desc: "Developed Smart Sense Report system with Node.js and Sails.js, detect employee productivity.",
+    },
+    {
+      id: "004",
+      title: "Graduation",
+      company: "Maharaja Sayajirao University, Vadodara, Gujrat",
+      year: "2019",
+      companyLink: "https://www.msubaroda.ac.in/",
+      desc: "Major in Computer Science and Engineering with a CGPA of 5.0. ",
+    },
+    {
+      id: "005",
+      title: "High School",
+      company: "Anand High School, Anand.",
+      year: "2014",
+      companyLink: "https://www.srksm.org/highschool",
+      desc: "Subjects being PCM - I barely survived with 50.5% marks aggregate.",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1IaNQGWyRfSZCdiuxhsfOgr8yIFUBz79U/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/aniket28__",
+    twitter: "https://twitter.com/Aniketpanchal28",
+    linkedin: "https://www.linkedin.com/in/aniket-panchal-b11720108",
+    github: "https://github.com/aapanchal",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
