bab167f496190f4fe98bd15b93472393d80cce03
Aniket Panchal <39687408+aapanchal@users.noreply.github.com>
2024-12-19 12:16:53 +0530

    update

--- components/ContainerBlock.js
+++ components/ContainerBlock.js
@@ -30,12 +30,12 @@ export default function ContainerBlock({
             href={``}
           />
           <meta property="og:type" content={meta.type} />
-          <meta property="og:site_name" content="Manu Arora" />
+          <meta property="og:site_name" content="Aniket panchal" />
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
           <meta property="og:image" content={meta.image} />
           <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@mannupaaji" />
+          <meta name="twitter:site" content="@Aniketpanchal28" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
           <meta name="twitter:image" content={meta.image} />
